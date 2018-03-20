5ed6ba7df5d75c604d4141dcd3f743a1c1285947
Lorenzo <lorhernandez@csumb.edu>
2018-03-20 16:02:30 -0700

    String concatenation

---
+++ Chapter 2/2-7.py
@@ -0,0 +1,11 @@
+# let's put some strings together
+first = 'lorenzo'
+last = 'hernandez'
+full = first + ' ' + last
+
+# prints 'lorenzo hernandez'
+print(full)
+
+# let's say hello with a variable!
+hello = 'Hello, ' + full.title() + '!'
+print(hello)
