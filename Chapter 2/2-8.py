58c9e4bcd2f8ecc8ad6323defe4ab905287d3936
Lorenzo <lorhernandez@csumb.edu>
2018-03-20 16:08:10 -0700

    working with newline and tab additions to strings

---
+++ Chapter 2/2-8.py
@@ -0,0 +1,13 @@
+# How about some whitespace addition to strings?
+
+# Let's just see Python.
+print("Python")
+
+# Now let's tab that over using \t
+print("\tPython")
+
+# How about inserting some newlines using \n
+print("Languages:\nPython\nC\nJavascript")
+
+# Put them both together and what do you get?
+print("Languages:\n\tPython\n\tC\n\tJavascript")
