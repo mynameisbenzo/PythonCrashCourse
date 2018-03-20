37320f7236967d465c9683a8d6b981ab15dfbb4b
Lorenzo <lorhernandez@csumb.edu>
2018-03-20 15:57:59 -0700

    Some string methods upper, lower, title

---
+++ Chapter 2/2-6.py
@@ -0,0 +1,15 @@
+# String methods
+name = "lorenzo hernandez"
+
+# prints 'Lorenzo Hernandez'
+print(name.title())
+
+name = name.upper()
+
+# prints 'LORENZO HERNANDEZ'
+print(name)
+
+name = name.lower()
+
+# prints 'lorenzo hernandez'
+print(name)
