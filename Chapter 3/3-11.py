72c881350621bf015053305a35cf6315b004bca7
Lorenzo <lorhernandez@csumb.edu>
2018-03-20 23:11:58 -0700

    It hurts to make mistakes on purpose sometimes

---
+++ Chapter 3/3-11.py
@@ -0,0 +1,5 @@
+# 3-11: an intentional error?  If that's what you want, then let's make it happen.  T_T
+myList = ['dog','cat','frog','antelope']
+
+# the following line is going to spit out an index out of range error
+print(myList[4])
