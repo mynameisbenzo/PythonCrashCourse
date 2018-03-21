6d610382d512bb46d39eeace02a24a78934a49fa
Lorenzo <lorhernandez@csumb.edu>
2018-03-20 20:52:58 -0700

    intro to lists

---
+++ Chapter 3/3-01_to_3-03.py
@@ -0,0 +1,18 @@
+# 3-1: Let's make a list! ... of names
+
+names = ['Lorenzo','Arlette','Aaron']
+
+# 3-2: use the list of names and say hi!
+
+msgFront = 'Hello, '
+msgBack = '!'
+print(msgFront + names[0] + msgBack)
+print(msgFront + names[1] + msgBack)
+print(msgFront + names[2] + msgBack)
+
+# 3-3: book says to make a list of your favorite transportation but I'm not feeling that
+
+places = ['Japan', 'The Moon', 'New Zealand']
+message = 'I\'d like to go to '
+
+print(message + places[0] + ', ' + places[1] + ', and ' + places[2])
