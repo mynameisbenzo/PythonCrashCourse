86812617a4476048960cdf6e22929434e4815c3b
Lorenzo <lorhernandez@csumb.edu>
2018-03-21 00:50:41 -0700

    For loops unlocked

---
+++ Chapter 4/4-01_to_4-02.py
@@ -0,0 +1,18 @@
+'''
+	4-1: Please don't judge my taste in pizza, because
+	we finally get to use for loops!
+'''
+pizzas = ['Hawaiian','Pizzas with Feta Cheese on them','Stuffed Crust']
+for pizza in pizzas:
+	print('I would love to eat a ' + pizza + ' right about now.')
+print('...I am really craving pizza.')
+
+'''
+	4-2: The goal is to print out animals that are similar.
+	Doesn't say they have to exist.... actually... that would
+	make them similar!
+'''
+animalsThatExist = ['Chimera','Goomba','Bulbasaur']
+for animal in animalsThatExist:
+	print('I definitely saw a ' + animal + ' yesterday and I am 100% not lying.')
+print('All of these animals definitely exist in some way or form')
