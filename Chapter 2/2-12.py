e504ef706be7848893935651b594e54df24b8b8f
Lorenzo <lorhernandez@csumb.edu>
2018-03-20 20:34:54 -0700

    comments and how to make them

---
+++ Chapter 2/2-12.py
@@ -0,0 +1,11 @@
+# quick note for comments
+#	# 		= comment out one line
+#   """ """	= comment out more than one line
+
+# Example of one line comment
+
+"""
+	Example of 
+	multi-line
+	comment
+""" 
