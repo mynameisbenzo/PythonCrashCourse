6021b05b2814572bb40931fe64bbc5733cf7d6d0
Lorenzo <lorhernandez@csumb.edu>
2018-03-22 14:42:56 -0700

    Code review, spacing, and the future

---
+++ Chapter 5/5-12_to_5-13.py
@@ -0,0 +1,11 @@
+'''
+	5-12: This exercise is about taking a second to go
+	back and review code from previous exercise to 
+	insure conditional statements are spaced properly.
+'''
+'''
+	5-13:  ...and this one is to take a bit of time to
+	jot down any ideas you may have for future programs!
+	If it's all the same to you, then I'll just be writing
+	down my ideas.
+'''
