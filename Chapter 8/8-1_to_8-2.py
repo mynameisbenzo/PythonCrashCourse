f4bc5814bd5d6f43e72cb3a0c200ee8a7255cf3b
Lorenzo <lorhernandez@csumb.edu>
2018-03-23 12:02:32 -0700

    Introduction to functions

---
+++ Chapter 8/8-1_to_8-2.py
@@ -0,0 +1,18 @@
+'''
+	8-1: Functions 101 - stop copy-pasting
+	and just reuse that code!  To start,
+	let's just say hi.
+'''
+def hello():
+	print("Hello functions!")
+hello()
+
+'''
+	8-2: Functions 102 - Parameters and what
+	they do.  Just saying hi is great and all
+	but let's pass someone's name so we can 
+	say hi to specific people.
+'''
+def helloYou(name):
+	print("Hello " + name + "!")
+helloYou("Lorenzo")
