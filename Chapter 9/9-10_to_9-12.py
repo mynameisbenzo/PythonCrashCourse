51e1dedaedb93bc4b107c98a25f0dec09edabbe6
Lorenzo <lorhernandez@csumb.edu>
2018-03-23 23:00:59 -0700

    retreading the steps already taken

---
+++ Chapter 9/9-10_to_9-12.py
@@ -0,0 +1,11 @@
+'''
+	For the following exercises, the goal
+	is to import classes into the current 
+	python script.  The things is... we've
+	already done that!  For each of the 
+	classes being asked to be imported!
+
+	9-10: Restaurant
+	9-11: User
+	9-12: Admin, Privilege
+'''
