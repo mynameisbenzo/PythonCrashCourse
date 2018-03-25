447a9a1fb6bfb29eb935ccc0b16699b93294c096
Lorenzo <lorhernandez@csumb.edu>
2018-03-24 17:11:17 -0700

    changing the background color

--- Projects/Project 1 - Alien Invasion/alien_invasion.py
+++ Projects/Project 1 - Alien Invasion/alien_invasion.py
@@ -11,12 +11,18 @@ def run_game():
 	screen = pygame.display.set_mode((1200,800))
 	pygame.display.set_caption("Alien Invasion")
 	
+	# set background color
+	bg_color = (230,230,230)
+	
 	# start the game loop
 	while True:
 		# look for keyboard and mouse events
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
+				
+		# redraw screen
+		screen.fill(bg_color)
 				
 		# display screen
 		pygame.display.flip()
