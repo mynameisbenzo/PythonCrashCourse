889946b6c64cd7656a6af8809d43776eba23a4d2
Lorenzo <lorhernandez@csumb.edu>
2018-04-08 17:15:02 -0700

    smaller points and more of them being plotted

--- Projects/Project 2 - Data Visualization/rw_visual.py
+++ Projects/Project 2 - Data Visualization/rw_visual.py
@@ -5,12 +5,12 @@ from random_walk import RandomWalk
 #keep making new walks if the program is active
 while True:
 	# make a random walk and plot points
-	rw = RandomWalk()
+	rw = RandomWalk(50000)
 	rw.fill_walk()
 
 	point_numbers = list(range(rw.num_points))
 	plt.scatter(rw.x_values, rw.y_values, c=point_numbers, 
-		cmap=plt.cm.Blues, edgecolor='none', s=15)
+		cmap=plt.cm.Blues, edgecolor='none', s=1)
 		
 	# emphasize first and last points
 	plt.scatter(0, 0, c='green', edgecolors='none', s=100)
