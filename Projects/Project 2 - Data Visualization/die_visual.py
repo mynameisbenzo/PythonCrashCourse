a266027af76565ad3a70de0831d663772bd79589
Lorenzo <lorhernandez@csumb.edu>
2018-04-08 18:03:17 -0700

    numbers are fine and dandy but graphs look way better

--- Projects/Project 2 - Data Visualization/die_visual.py
+++ Projects/Project 2 - Data Visualization/die_visual.py
@@ -1,3 +1,5 @@
+import pygal
+
 from die import Die
 
 # create a d6
@@ -15,4 +17,13 @@ for value in range(1, die.num_sides+1):
 	frequency = results.count(value)
 	frequencies.append(frequency)
 	
-print(frequencies)
+# visualize results
+hist = pygal.Bar()
+
+hist.title = "Results of rolling one d6 1000 times"
+hist.x_labels = [str(x) for x in range(1,7)]
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
