df3f593c9da4cd5f5273b209e68ab54f927fd4b6
Lorenzo <lorhernandez@csumb.edu>
2018-04-08 17:59:11 -0700

    increase number of rolls and print frequency of occurences

--- Projects/Project 2 - Data Visualization/die_visual.py
+++ Projects/Project 2 - Data Visualization/die_visual.py
@@ -5,8 +5,14 @@ die = Die()
 
 # roll and store the results
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
 	result = die.roll()
 	results.append(result)
 	
-print(results)
+# analyze results
+frequencies = []
+for value in range(1, die.num_sides+1):
+	frequency = results.count(value)
+	frequencies.append(frequency)
+	
+print(frequencies)
