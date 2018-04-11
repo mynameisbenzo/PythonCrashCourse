afc63eb7306d9b0fea8b733024d84f2ba1fb6c2b
Lorenzo <lorhernandez@csumb.edu>
2018-04-10 21:18:26 -0700

    getting a grip on histograms.  creating svgs

--- Projects/Project 2 - Data Visualization/die_visual.py
+++ Projects/Project 2 - Data Visualization/die_visual.py
@@ -3,27 +3,28 @@ import pygal
 from die import Die
 
 # create a d6
-die = Die()
+die_1 = Die()
+die_2 = Die()
 
 # roll and store the results
 results = []
 for roll_num in range(1000):
-	result = die.roll()
+	result = die_1.roll() + die_2.roll()
 	results.append(result)
 	
 # analyze results
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(1, max_result+1):
 	frequency = results.count(value)
 	frequencies.append(frequency)
 	
 # visualize results
 hist = pygal.Bar()
-
 hist.title = "Results of rolling one d6 1000 times"
-hist.x_labels = [str(x) for x in range(1,7)]
+hist.x_labels = [str(x) for x in range(1,max_result+1)]
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
-hist.add('D6', frequencies)
+hist.add('D6 + D6', frequencies)
 hist.render_to_file('die_visual.svg')
