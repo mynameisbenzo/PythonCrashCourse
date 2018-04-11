5912bc380756121dfdee5dd9436ffdb4b67682d7
Lorenzo <lorhernandez@csumb.edu>
2018-04-10 21:20:19 -0700

    different sided die rolled together (D6, D10)

--- Projects/Project 2 - Data Visualization/die_visual.py
+++ Projects/Project 2 - Data Visualization/die_visual.py
@@ -4,11 +4,11 @@ from die import Die
 
 # create a d6
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 # roll and store the results
 results = []
-for roll_num in range(1000):
+for roll_num in range(50000):
 	result = die_1.roll() + die_2.roll()
 	results.append(result)
 	
@@ -26,5 +26,5 @@ hist.x_labels = [str(x) for x in range(1,max_result+1)]
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
-hist.add('D6 + D6', frequencies)
+hist.add('D6 + D10', frequencies)
 hist.render_to_file('die_visual.svg')
