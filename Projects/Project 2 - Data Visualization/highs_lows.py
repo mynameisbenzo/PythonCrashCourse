39676032869bcb9e9c424168ecbb22d57151be9d
Lorenzo <lorhernandez@csumb.edu>
2018-04-12 22:47:54 -0700

    getting highs from dataset

--- Projects/Project 2 - Data Visualization/highs_lows.py
+++ Projects/Project 2 - Data Visualization/highs_lows.py
@@ -2,9 +2,13 @@ import csv
 
 filename = 'sitka_weather_07-2014.csv'
 
+# get high temperatures from file
 with open(filename) as f:
 	reader = csv.reader(f)
 	header_row = next(reader)
 	
-	for index, column_header in enumerate(header_row):
-		print(index, column_header)
+	highs = []
+	for row in reader:
+		highs.append(row)
+		
+	print(highs)
