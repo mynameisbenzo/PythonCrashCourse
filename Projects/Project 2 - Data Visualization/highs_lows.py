eaf69ed992abc617b3ad21614b2b0102fa94850d
Lorenzo <lorhernandez@csumb.edu>
2018-04-12 23:07:06 -0700

    adding error checking for improper or missing dates in file

--- Projects/Project 2 - Data Visualization/highs_lows.py
+++ Projects/Project 2 - Data Visualization/highs_lows.py
@@ -4,7 +4,8 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 
 # filename = 'sitka_weather_07-2014.csv'
-filename = 'sitka_weather_2014.csv'
+# filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 
 # get dates, high, and low temperatures from file
 with open(filename) as f:
@@ -13,14 +14,16 @@ with open(filename) as f:
 	
 	dates, highs, lows = [], [], []
 	for row in reader:
-		current_date = datetime.strptime(row[0], "%Y-%m-%d")
-		dates.append(current_date)
-	
-		high = int(row[1])
-		highs.append(high)
-		
-		low = int(row[3])
-		lows.append(low)
+		try:
+			current_date = datetime.strptime(row[0], "%Y-%m-%d")	
+			low = int(row[3])
+			high = int(row[1])
+		except ValueError:
+			print(current_date, 'missing data')
+		else:
+			dates.append(current_date)
+			highs.append(high)
+			lows.append(low)
 		
 	# plot data
 	fig = plt.figure(dpi=128, figsize=(10,6))
@@ -29,7 +32,8 @@ with open(filename) as f:
 	plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 	
 	# format plot
-	plt.title("Daily high and low temperatures - 2014", fontsize=24)
+	plt.title("Daily high and low temperatures - 2014\n" + 
+		"Death Valley, CA", fontsize=20)
 	plt.xlabel('', fontsize=16)
 	fig.autofmt_xdate()
 	plt.ylabel("Temperature (F)", fontsize=16)
