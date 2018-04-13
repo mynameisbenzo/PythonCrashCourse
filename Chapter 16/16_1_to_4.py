c4037e3648319eb7caa6dfc06c1f05d5700919e5
Lorenzo <lorhernandez@csumb.edu>
2018-04-12 23:19:43 -0700

    starting first exercises in chapter 16

---
+++ Chapter 16/16_1_to_4.py
@@ -0,0 +1,43 @@
+'''
+	16-1:
+		Goal:
+			Do what was done with data from Sitka and
+			Death Valley for San Francisco data
+	16-2:
+		Goal:
+			Make a direct comparison between Sitka and
+			Death Valley
+	16-3:
+		Goal:
+			Visualize the rainfall on a location of my
+			choice.  First, for a month and then for
+			the entire year.
+	16-4:
+		Goal:
+			Extra visualizations for the fun of it.
+			
+	Approach:
+		All problem solutions will be contained in this
+		file, so it may be rather long.  
+		
+		Problem 1, I believe, should be very straight-
+		forward.  The methods already done are there for
+		me to implement, so it's a matter of repeating or
+		reusing previous code.
+		
+		Problem 2 shouldn't be too difficult either.  In
+		the previous example, to note different data being
+		mapped the colors of the data were different colors.
+		An issue brought up in the book is that determining
+		the Y-values on the plot will be something to consider.
+		I believe finding the lowest and highest overal temp-
+		eratures from either dataset would be the resolution.
+		
+		Problem 3 allows me to ditch high/low temperatures in 
+		favor of a new parameter to visualize: rainfall.  The
+		obvious go to here would be Seattle in my opinion.
+		
+		Problem 4 Free reign on visualization.  My choice is 
+		Seattle rainfall vs the rainfall of somewhere really
+		dry.  I'll have to look that up.
+'''
