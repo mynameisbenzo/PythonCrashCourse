254f4ca92964809d63a4a8fac8b479f0f22cb2ce
Lorenzo <lorhernandez@csumb.edu>
2018-04-12 22:52:51 -0700

    plotted highs of july 2014 using matplotlib

--- Projects/Project 2 - Data Visualization/highs_lows.py
+++ Projects/Project 2 - Data Visualization/highs_lows.py
@@ -1,5 +1,7 @@
 import csv
 
+from matplotlib import pyplot as plt
+
 filename = 'sitka_weather_07-2014.csv'
 
 # get high temperatures from file
@@ -12,4 +14,14 @@ with open(filename) as f:
 		high = int(row[1])
 		highs.append(high)
 		
-	print(highs)
+	# plot data
+	fig = plt.figure(dpi=128, figsize=(10,6))
+	plt.plot(highs, c='red')
+	
+	# format plot
+	plt.title("Daily high temperatures, July 2014", fontsize=24)
+	plt.xlabel('', fontsize=16)
+	plt.ylabel("Temperature (F)", fontsize=16)
+	plt.tick_params(axis='both', which='major', labelsize=16)
+	
+	plt.show()
