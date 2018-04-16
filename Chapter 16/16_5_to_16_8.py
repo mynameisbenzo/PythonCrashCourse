a5b3561616cbdb4bd2a812e82e8bc6383d78d66c
Lorenzo <lorhernandez@csumb.edu>
2018-04-15 22:25:41 -0700

    problem 16-5 solved

---
+++ Chapter 16/16_5_to_16_8.py
@@ -0,0 +1,31 @@
+'''
+	16-5:
+		Goal:
+			There are some countries in that cannot
+			be found in the COUNTRIES dictionary.
+			Determine what those countries are and
+			return the correct country_code.
+	16-6:
+		Goal:
+			Create a world map that displays the GDP
+			of each country with the link given in 
+			the book.
+	16-7:
+		Goal:
+			Choose my own data to display on the world
+			map for each country.
+	16-8:
+		Goal:
+			Write a proper test for the first problem 
+			16-5.
+			
+	Approach:
+		Problem 5 will be addressed in the projects 
+		section and not here.
+		
+		Problem 6 will be addressed here as well as
+		Problem 7.
+		
+		Problem 8 will also be addressed in the projects
+		section.
+'''
