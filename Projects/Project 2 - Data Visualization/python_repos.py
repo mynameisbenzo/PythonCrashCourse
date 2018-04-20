a5ac692ca5265d70996f08076f33378a008782fa
Lorenzo <lorhernandez@csumb.edu>
2018-04-19 22:16:45 -0700

    adjusting api request to include custom tooltips

--- Projects/Project 2 - Data Visualization/python_repos.py
+++ Projects/Project 2 - Data Visualization/python_repos.py
@@ -15,10 +15,18 @@ print("Total repositories: ", response_dict['total_count'])
 # explore information about the repositories
 repo_dicts = response_dict['items']
 
-names, stars = [],[]
+names, plot_dicts = [],[]
 for repo_dict in repo_dicts:
 	names.append(repo_dict['name'])
-	stars.append(repo_dict['stargazers_count'])
+	
+	# check if description exists to prevent errors
+	if repo_dict['description'] == None:
+		continue
+	plot_dict = {
+		'value':repo_dict['stargazers_count'],
+		'label':repo_dict['description'],
+	}
+	plot_dicts.append(plot_dict)
 	
 # make a visualization
 my_style = LS('#333366', base_style=LCS)
@@ -37,5 +45,5 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on Github'
 chart.x_labels = names
 
-chart.add('',stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
