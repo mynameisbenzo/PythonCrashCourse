8d5f6a8438d56eb5be46ce4df4441132e3add853
Lorenzo <lorhernandez@csumb.edu>
2018-04-21 10:53:00 -0700

    unregisted change for model.  models file not saved

--- Projects/Project 3 - Django/learning_log/learning_logs/models.py
+++ Projects/Project 3 - Django/learning_log/learning_logs/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Topic(models.Model):
 	text = models.CharField(max_length=200)
 	date_added = models.DateTimeField(auto_now_add=True)
+	
+	''' Return a string representation of the model '''
+	def __str__(self):
+		return self.text
