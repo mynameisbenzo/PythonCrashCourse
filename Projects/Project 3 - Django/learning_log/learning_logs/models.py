a713a7119ab6f7ea143e9831cbb83ca48c2095c4
Lorenzo <lorhernandez@csumb.edu>
2018-04-21 11:21:12 -0700

    creating many-to-one relationship between two models.  book had deprecated method. updated version used.

--- Projects/Project 3 - Django/learning_log/learning_logs/models.py
+++ Projects/Project 3 - Django/learning_log/learning_logs/models.py
@@ -8,3 +8,25 @@ class Topic(models.Model):
 	''' Return a string representation of the model '''
 	def __str__(self):
 		return self.text
+		
+''' something specific learned about a topic '''
+class Entry(models.Model):
+	'''
+		The book version is deprecated.  Not only
+		do you have to give the model for the 
+		ForeignKey, but you also have to pass the
+		what will happen when or if the entry is
+		deleted.
+	'''
+	topic = models.ForeignKey(
+		Topic,
+		on_delete=models.CASCADE)
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add=True)
+	
+	class Meta:
+		verbose_name_plural = 'entries'
+		
+	''' return a string representation of the model '''
+	def __str__(self):
+		return self.text[:50] + "..."
