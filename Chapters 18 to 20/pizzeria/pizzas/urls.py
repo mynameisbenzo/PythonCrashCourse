83c35ca3f39401c6ad4e22c0ebf11f9cdbb10a21
Lorenzo <lorhernandez@csumb.edu>
2018-04-22 13:49:43 -0700

    problem 18-8 completed.  two pages added that show pizzas and toppings of selected pizzas

--- Chapters 18 to 20/pizzeria/pizzas/urls.py
+++ Chapters 18 to 20/pizzeria/pizzas/urls.py
@@ -5,4 +5,10 @@ from . import views
 urlpatterns = [
 	# home page
 	path('', views.index, name='index'),
+	
+	# list of pizzas
+	path('pizzas/$', views.pizzas, name='pizzas'),
+	
+	# single pizza with detailed info
+	path('pizzas/<pizza_id>/$', views.pizza, name='pizza'),
 ]
--- Chapters 18 to 20/pizzeria/pizzas/views.py
+++ Chapters 18 to 20/pizzeria/pizzas/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 
+from .models import Pizza
 
+'''
+	home page
+'''
 def index(request):
 	return render(request, 'pizzas/index.html')
+	
+'''
+	list of pizza names
+'''
+def pizzas(request):
+	pizzas = Pizza.objects.order_by('date_added')
+	context = {'pizzas':pizzas}
+	return render(request, 'pizzas/pizzas.html', context)
+	
+'''
+	single pizza and the toppings for said pizza
+'''
+def pizza(request, pizza_id):
+	pizza = Pizza.objects.get(id=pizza_id)
+	toppings = pizza.topping_set.order_by('-date_added')
+	context = {'pizza':pizza, 'toppings':toppings}
+	return render(request, 'pizzas/pizza.html', context)
