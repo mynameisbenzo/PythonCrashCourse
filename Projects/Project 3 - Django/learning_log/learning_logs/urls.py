cd3c9e1e080271fbc793e0321deffea8d36d3f7d
Lorenzo <lorhernandez@csumb.edu>
2018-04-22 12:57:16 -0700

    show us the topics we've been learning so far

--- Projects/Project 3 - Django/learning_log/learning_logs/urls.py
+++ Projects/Project 3 - Django/learning_log/learning_logs/urls.py
@@ -12,4 +12,7 @@ app_name = "learning_logs"
 urlpatterns = [
 	# Home page
 	path('', views.index, name='index'),
+	
+	# show all topics
+	path('/topics/$', views.topics, name='topics'),
 ]
--- Projects/Project 3 - Django/learning_log/learning_logs/views.py
+++ Projects/Project 3 - Django/learning_log/learning_logs/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 '''
 	the home page for learning log
 '''
 def index(request):
 	print("ayy")
 	return render(request, 'learning_logs/index.html')
+	
+'''
+	show all topics
+'''
+def topics(request):
+	topics = Topic.objects.order_by('date_added')
+	context = {'topics': topics}
+	return render(request, 'learning_logs/topics.html', context)
