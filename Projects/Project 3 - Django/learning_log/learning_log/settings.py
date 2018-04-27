c6e308777b1d323c888fb4aed3db2091ff20fb18
Lorenzo <lorhernandez@csumb.edu>
2018-04-27 00:25:44 -0700

    preparing for heroku

--- Projects/Project 3 - Django/learning_log/learning_log/settings.py
+++ Projects/Project 3 - Django/learning_log/learning_log/settings.py
@@ -133,3 +133,23 @@ LOGIN_URL = '/users/login/'
 BOOTSTRAP3 = {
 	'include_jquery': True,
 }
+
+# heroku settings
+if os.getcwd() == '/app':
+	import dj_database_url
+	DATABASES = {
+		'default':dj_database_url.config(default='postgres://localhost')
+	}
+	
+	# honor the 'x-forwarded-proto' header request.is_secure()
+	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PHOTO', 'https')
+	
+	# allow all host headers
+	ALLOWED_HOSTS = ['']
+	
+	# static asset configuration
+	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+	STATIC_ROOT = 'staticfiles'
+	STATICFILES_DIRS =(
+		os.path.join(BASE_DIR, 'static')
+	)
